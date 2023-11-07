af5e407e4af178c5bb6215ee841a7cfd5432dbda
MaxKuz67 <35179607+MaxKuz67@users.noreply.github.com>
2023-11-07 16:46:46 +0300

    Update StoreController.php

--- app/Http/Controllers/Role/StoreController.php
+++ app/Http/Controllers/Role/StoreController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\Role\StoreRequest;
 use App\Http\Resources\RoleResource;
 use Illuminate\Http\Request;
 use App\Services\Role\Service;
-
+#[\App\Attributes\Access(roles: ["admin"])]
 class StoreController extends BaseController
 {
     public function __construct()
