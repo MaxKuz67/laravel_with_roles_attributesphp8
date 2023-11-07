058216f91768a6f4f88e38c80c943a9d6d8fe1f0
MaxKuz67 <35179607+MaxKuz67@users.noreply.github.com>
2023-11-07 16:45:45 +0300

    Update StoreController.php

--- app/Http/Controllers/Role/StoreController.php
+++ app/Http/Controllers/Role/StoreController.php
@@ -7,9 +7,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Role\StoreRequest;
 use App\Http\Resources\RoleResource;
 use Illuminate\Http\Request;
+use App\Services\Role\Service;
 
 class StoreController extends BaseController
 {
+    public function __construct()
+    {
+        parent::__construct(new Service());
+    }
 
     public function __invoke(StoreRequest $request)
     {
